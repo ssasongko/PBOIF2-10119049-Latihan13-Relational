d04539b6f9edcc7d0df9d668754986442492722f
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:46:08 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan13-Relational/src/pbo/if210119049/latihan13/relational/PBOIF210119049Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.if210119049.latihan13.relational;
+
+/**
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program menampilkan operasi relasional.
+ */
+public class PBOIF210119049Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b =  " + (bil1 == bil2));
+        System.out.println("a != b =  " + (bil1 != bil2));
+        System.out.println("a > b =  " + (bil1 > bil2));
+        System.out.println("a < b =  " + (bil1 < bil2));
+        System.out.println("b >= a =  " + (bil1 >= bil2));
+        System.out.println("b <= a =  " + (bil1 <= bil2));
+    }
+    
+}
